9983c79b7a3aa2044959d5b16247d3b0c7391d3a
Alexander Shashkov <45598701+AlexShashkov@users.noreply.github.com>
2020-12-23 20:53:34 +0300

    Add files via upload

--- lec2/6.jl
+++ lec2/6.jl
@@ -1,50 +1,80 @@
 ENV["MPLBACKEND"]="tkagg"
 using PyPlot
 using HorizonSideRobots
-
-include("robot_operations.jl")
 pygui(true) 
 
 #           up  down   right left
 #sides = (Nord, Sud, Ost, West)
 
-function mark_innerrectangle_perimetr(r::Robot)
-    num_steps=fill(0,3) # - вектор-столбец из 3-х нулей
-    for (i,side) in enumerate((Sud,West,Sud))
-        num_steps[i]=moveSide!(r,side)
-    end
-    #УТВ: Робот - в Юго-западном углу внешней рамки
+function main(r::Robot)
+    path = goToTheCorner(Sud, West)
 
-    side = find_border!(r,Ost,side)
-    #УТВ: Робот - у западной границы внутренней перегородки
+    findBorder(r)
+    markBorder(r)
 
-    mark_innerrectangle_perimetr!(r,side)
-    #УТВ: Робот - снова у западной границы внутренней прямоугольной перегородки
-
-    moveSide!(r,Sud)
-    moveSide!(r,West)
-    #УТВ: Робот - в Юго-западном улу внешней рамки
-
-    for (i,side) in enumerate((Nord,Ost,Nord))
-        followByCount!(r,side, num_steps[i])
-    end
-    #УТВ: Робот - в исходном положении
-end
-
-function mark_innerrectangle_perimetr!(r::Robot, side::HorizonSide)
-    direction_of_movement, direction_to_border = get_directions(side)
-    for i ∈ 1:4
-        putmarkers!(r, direction_of_movement[i], direction_to_border[i]) 
+    goToTheCorner(Sud, West)
+    for item in path[end:-1:1]
+        print(item)
+        followByCountRec(r, item[1], item[2])
     end
 end
 
-get_directions(side::HorizonSide) = 
-    if side == Nord  
-    # - обход будет по часовой стрелке      
-        return (Nord,Ost,Sud, West), (Ost,Sud,West,Nord)
-    else # - обход будет против часовой стрелки
-        return (Sud,Ost,Nord,West), (Ost,Nord,West,Sud) 
+function findBorder(r)
+    while(!isborder(r, Nord))
+        if(!isborder(r, Ost))
+            move!(r, Ost)
+        else
+            moveSide(r, West)
+            move!(r, Nord)
+        end
     end
+end
+
+function markBorder(r)
+    comp = Dict(
+        Nord => Ost,
+        West => Nord,
+        Sud => West,
+        Ost => Sud
+    )
+    for side ∈ [Nord, West, Sud, Ost]
+        while isborder(r, side)
+            putmarker!(r)
+            move!(r, comp[side])
+        end
+        putmarker!(r)
+        move!(r, side)
+    end
+end
+
+function goToTheCorner(side1::HorizonSide, side2::HorizonSide)
+    pathBack = []
+    while isborder(r, side1) != true || isborder(r, side2) != true
+        stepsToX = moveSide(r, side1)
+        stepsToY = moveSide(r, side2)
+        push!(pathBack, [inverseSide(side1), stepsToX])
+        push!(pathBack, [inverseSide(side2), stepsToY])
+    end
+
+    return pathBack
+end
+
+function moveSide(r::Robot, side::HorizonSide)
+    # Move untill you find pr
+    steps = 0
+    while isborder(r, side) != true
+        move!(r, side)
+        steps += 1
+    end
+    return steps
+end
+
+function followByCount(r::Robot, side::HorizonSide, count::Int)
+    for i = 1:count
+        moveStep(r, side, false)
+    end
+end
+
 
 r=Robot(animate=true)
-mark_innerrectangle_perimetr(r)
+main(r)
